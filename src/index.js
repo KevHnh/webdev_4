0b7347b8facb528667f3ca8dd31c085c198a509e
KevHnh <kevinhinh@DESKTOP-UVTIUFR.localdomain>
2022-04-18 16:07:12 -0400

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import InitialApp from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <InitialApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
